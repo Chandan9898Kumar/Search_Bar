4a62873789f1317fd65db9871a3edcf74372b287
Chandan <Chandan17ee@gmail.com>
2023-04-08 00:13:12 +0530

    feat : completed emi calculator

--- src/EmiCalculator/MainEmi.js
+++ src/EmiCalculator/MainEmi.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import "./emiStyle.css";
 import InputText from "./Component/InputData";
 
+import { tenureData } from "./utils/Constants";
+import { numberWithCommas } from "./utils/Config";
+
 const MainEmiPage = () => {
   const [cost, setCost] = useState(0);
   const [interest, setInterest] = useState(10);
@@ -16,6 +19,24 @@ const MainEmiPage = () => {
     if (!cost) {
       return;
     }
+
+    const pAmount = cost - dPaymentValue;
+    const rateOfInterest = interest / 100;
+    const numOfYears = tenure / 12;
+
+    const EMI =
+      (pAmount * rateOfInterest * (1 + rateOfInterest) ** numOfYears) /
+      ((1 + rateOfInterest) ** numOfYears - 1);
+
+    return Number(EMI / 12).toFixed(0);
+  };
+
+  const calculateDP = (emi) => {
+    if (!cost) {
+      return;
+    }
+    const downPaymentPercentage = 100 - (emi / calculateEmi(0)) * 100;
+    return Number((downPaymentPercentage / 100) * cost).toFixed(0);
   };
 
   const updateEmi = (event) => {
@@ -28,7 +49,25 @@ const MainEmiPage = () => {
     setEmi(emiData);
   };
 
-  const updateDownPayment = () => {};
+  const updateDownPayment = (event) => {
+    if (!cost) {
+      return;
+    }
+    const emi = Number(event.target.value);
+    setEmi(emi.toFixed(0));
+    const downPay = calculateDP(emi);
+    setDownPayment(downPay);
+  };
+
+  const totalDownPayment = () => {
+    return numberWithCommas(
+      (Number(downPayment) + (cost - downPayment) * (fee / 100)).toFixed(0)
+    );
+  };
+
+  const totalEmi = () => {
+    return numberWithCommas((emi * tenure).toFixed(0));
+  };
 
   return (
     <>
@@ -61,6 +100,9 @@ const MainEmiPage = () => {
             <div style={{ textAlign: "left", marginLeft: "90px" }}>
               Down Payment
             </div>
+            <div style={{ textAlign: "left", marginLeft: "90px" }}>
+              {`Total Down Payment -${totalDownPayment()}`}
+            </div>
             <span>0%</span>
             <input
               type="range"
@@ -76,6 +118,9 @@ const MainEmiPage = () => {
             <div style={{ textAlign: "left", marginLeft: "90px" }}>
               Loan Per Month
             </div>
+            <div style={{ textAlign: "left", marginLeft: "90px" }}>
+              {`Total Loan Amount - ${totalEmi()}`}
+            </div>
             <span>0%</span>
             <input
               type="range"
@@ -85,6 +130,23 @@ const MainEmiPage = () => {
               onChange={updateDownPayment}
             />
             <span>100%</span>
+          </div>
+          <div className="tenure">
+            <span>Tenure</span>
+            <div className="tenureContainer">
+              {tenureData.map((dataTenure) => {
+                return (
+                  <button
+                    className={
+                      dataTenure === tenure ? "activeButton" : "buttonStyle"
+                    }
+                    onClick={() => setTenure(dataTenure)}
+                  >
+                    {dataTenure}
+                  </button>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
