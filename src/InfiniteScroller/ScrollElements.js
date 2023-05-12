09c299adb98933d41554f6c6154e47d46f17b25c
Chandan <Chandan17ee@gmail.com>
2023-05-12 16:54:59 +0530

    chore: added all functionality

--- src/InfiniteScroller/ScrollElements.js
+++ src/InfiniteScroller/ScrollElements.js
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useRef, useCallback } from "react";
+import React, { memo, useEffect, useRef, useCallback, useState } from "react";
 
 const ScrollingElement = ({
   inputValue,
@@ -8,21 +8,85 @@ const ScrollingElement = ({
 }) => {
   // initialPage is 1
   const pageNumber = useRef(1);
+  const [loading, setLoading] = useState(false);
+
+  const observer = useRef(null);
 
   // whenever inputText is changed then we then our function should get called in UseEffect.
   //  Note :- here we have called fetchData() function above useEffect because in useEffect we have passed fetchData() function
   //  as an dependency.so if we called that function below useEffect then it will throw error which show can't access before initialization.
   const fetchData = useCallback(() => {
-    getData(inputValue, pageNumber.current);
+    setLoading(true);
+    getData(inputValue, pageNumber.current).finally(() => {
+      setLoading(false);
+    });
   }, [inputValue]);
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
 
-  return(
-  <>
+  const lastElementObserver = useCallback((node) => {
+    // here node is referring to the last element of an array.
+    if (loading) {
+      return;
+    }
 
-  </>);
+    if (observer.current) {
+      observer.current.disconnect();
+    }
+    // The options object passed into the IntersectionObserver() constructor let you control
+    // the circumstances under which the observer's callback is invoked. It has the following fields: root , rootMargin , threshold
+    observer.current = new IntersectionObserver((entries) => {
+      // isIntersecting property is a Boolean value which is true if the target element intersects with the intersection observer's root.
+      //  If this is true, then, the IntersectionObserverEntry describes a transition into a state of intersection; if it's false, then you know the transition is from intersecting to not-intersecting.
+
+      if (entries[0].isIntersecting) {
+        pageNumber.current += 1;
+        fetchData();
+      }
+    });
+
+    //                                                 Targeting an element to be observed.
+    // Once you have created the observer, you need to give it a target element to watch:
+
+    if (node) {
+      observer.current.observe(node);
+    }
+    // the callback we setup for the observer will be executed now for the first time
+    // it waits until we assign a target to our observer (even if the target is currently not visible)
+
+    // Whenever the target meets a threshold specified for the IntersectionObserver,
+    // the callback is invoked. The callback receives a list of IntersectionObserverEntry objects and the observer:
+  });
+
+  const listItemRender = useCallback(() => {
+    // In ListData we are having 1k data,but only 100 data's are shown because our viewPort can show only that,
+    //  rest data will be hidden.so render all 1k data we are need infiniteScroller .
+    // we set condition on last index of an array.
+    return listData.map((item, index) => {
+      if (index === listData.length - 1) {
+        // when we are having last index of an array,then we passed lastElementObserver to renderListItem() as a ref
+        return renderListItem(item, index, lastElementObserver);
+      }
+      return renderListItem(item, index, null);
+    });
+  });
+
+  return (
+    <>
+      <div>
+        {listItemRender()}
+        {loading && "Loading"}
+      </div>
+    </>
+  );
 };
 export default memo(ScrollingElement);
+
+//                                                   Intersection observer concepts and usage
+
+// The Intersection Observer API allows you to configure a callback that is called when either of these circumstances occur:
+
+//  A target element intersects either the device's viewport or a specified element. That specified element is called the root element or root for the purposes of the Intersection Observer API.
+//  The first time the observer is initially asked to watch a target element.
--- src/InfiniteScroller/Scroller.js
+++ src/InfiniteScroller/Scroller.js
@@ -11,15 +11,45 @@ const MainComponent = () => {
     setSearchText(event.target.value);
   }, []);
 
-  const renderItem = useCallback(({ title }, key, ref) => (
-    <div ref={ref} key={key}>
-      {title}
+  //  will render item on ui.
+  const renderItem = useCallback(({ title }, key, lastElementObserver) => (
+    // here ref is lastElementObserver,which is from <ScrollingElement /> component
+    <div className="renderScrollItems" ref={lastElementObserver} key={key}>
+      <span className="titleOfScroller">{title}</span>
     </div>
   ));
 
-  const getData = useCallback((val,pageNumber) => {
+  const getData = useCallback((inputValue, pageNumber) => {
+    //  Here we did not pass any data inside resolve() and reject(),currently we don't need it,just performing asynchronous operation.
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (controllerRef.current) {
+          //  if we are calling getData() again and again,so it will abort all the previous network calls.
+          controllerRef.current.abort();
+        }
+        // new AbortController() - It can be used to abort not only fetch , but other asynchronous tasks as well.
+        controllerRef.current = new AbortController();
 
+        //  for this a fresh network call will be created.we passed the signal property of an AbortController as a fetch option.
+        // The fetch method knows how to work with AbortController. It will listen to abort events on signal
+        const promise = await fetch(
+          "https://openlibrary.org/search.json?" +
+            new URLSearchParams({
+              q: inputValue,
+              page: pageNumber,
+            }),
+          { signal: controllerRef.current.signal }
+        );
+
+        const data = await promise.json();
+        resolve();
+        setData((prevData) => [...prevData, ...data.docs]);
+      } catch (e) {
+        reject();
+      }
+    });
   }, []);
+
   return (
     <>
       <div className="containerOfScroller">
@@ -54,3 +84,24 @@ const MainComponent = () => {
   );
 };
 export default MainComponent;
+
+//                                                     The AbortController object
+
+// Create a controller:
+
+// let controller = new AbortController();
+
+// A controller is an extremely simple object.
+
+//     It has a single method abort(),
+//     And a single property signal that allows to set event listeners on it.
+
+// When abort() is called:
+
+//     controller.signal emits the "abort" event.
+//     controller.signal.aborted property becomes true.
+
+// Generally, we have two parties in the process:
+
+//     The one that performs a cancelable operation, it sets a listener on controller.signal.
+//     The one that cancels: it calls controller.abort() when needed.
