1078b0c2ee6464a55c87b600597b0e6005daac07
Chandan <Chandan17ee@gmail.com>
2023-06-26 22:25:44 +0530

    chore:added lazy and suspense component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar/NavBar";
 import MainFunctionalPage from "./FunctionalSearch/MainSearchPage";
@@ -5,24 +6,28 @@ import HomePage from "./HomePage/Home";
 import MainClassSearchPage from "./Classcomponentsearch/MainClassPage";
 import MainEmiPage from "./EmiCalculator/MainEmi";
 import CountdownTimer from "./CountDownTimer/CountTimer";
-import StopWatchApp from './StopWatch/WatchStop'
-import MainComponent from './InfiniteScroller/Scroller'
+import StopWatchApp from "./StopWatch/WatchStop";
+import MainComponent from "./InfiniteScroller/Scroller";
+
 function App() {
+  
+
   return (
     <>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/functional" element={<MainFunctionalPage />} />
-          <Route exact path="/Class" element={<MainClassSearchPage />} />
-          <Route exact path="/EmiCalculator" element={<MainEmiPage />} />
-          <Route exact path="/CountdownTimer" element={<CountdownTimer />} />
-          <Route exact path="/stopWatch" element={<StopWatchApp />} />
-          <Route exact path="/Scroller" element={<MainComponent />} />
-
-        </Routes>
-      </BrowserRouter>
+      <Suspense fallback={'Please Wait...'}>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/functional" element={<MainFunctionalPage />} />
+            <Route exact path="/Class" element={<MainClassSearchPage />} />
+            <Route exact path="/EmiCalculator" element={<MainEmiPage />} />
+            <Route exact path="/CountdownTimer" element={<CountdownTimer />} />
+            <Route exact path="/stopWatch" element={<StopWatchApp />} />
+            <Route exact path="/Scroller" element={<MainComponent />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </>
   );
 }
